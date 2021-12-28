7fd8c7bb978447c3986da26d3bccbec8df9dfc09
Raul dos Santos Moraes <rauldosS.moraes@gmail.com>
2021-12-28 16:04:37 -0300

    update installed apps

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'crispy_forms'
+    'crispy_forms',
+    'django.contrib.sites',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
